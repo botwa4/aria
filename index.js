6c40efbd8a045c467fb77dff58a80f28080a6244
botwa4 <74042194+botwa4@users.noreply.github.com>
2020-11-06 14:40:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const menu = require("./lib/menu.js");
 const donate = require("./lib/donate.js");
 const info = require("./lib/info.js");
 //
-const BotName = 'ST4RZ BOT 🤖'; // Nama Bot Whatsapp
-const instagramlu = 'https://instagram.com/bintang_nur_pradana'; // Nama Instagramlu cok
-const whatsapplu = '0819-4698-3575'; // Nomor whatsapplu cok
+const BotName = 'XnX Bot 🤖'; // Nama Bot Whatsapp
+const instagramlu = 'https://instagram.com/ini.aria'; // Nama Instagramlu cok
+const whatsapplu = '0895-2534-0368'; // Nomor whatsapplu cok
 const kapanbotaktif = '24 Jam'; // Kapan bot lu aktif
-const grupch1 = 'https://chat.whatsapp.com/FsAlnxqz6y2BhCQi5ayCLG'; // OFFICIAL GRUP LU 1
-const grupch2 = 'https://chat.whatsapp.com/KLW3UlFfeaH36Ucm5zRfCz'; // OFFICIAL GRUP LU 2
+const grupch1 = 'https://chat.whatsapp.com/IY7l7nccKqr0ojwtei6Dqb'; // OFFICIAL GRUP LU 1
+const grupch2 = 'https://chat.whatsapp.com/IY7l7nccKqr0ojwtei6Dqb'; // OFFICIAL GRUP LU 2
 //
 const
 {
